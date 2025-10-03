8c523fdeeb9fb9b668f859f9cfa42cec9fe92cf7
liuzhiwei03 <liuzhiwei03@baidu.com>
2025-10-03 15:14:21 +0800

    在调试对话的功能

--- run.py
+++ run.py
@@ -2,6 +2,9 @@
 import sys
 from wxcloudrun import app
 
+# 确保所有视图模块被导入
+import wxcloudrun.views
+
 # 启动Flask Web服务
 if __name__ == '__main__':
     app.run(host=sys.argv[1], port=sys.argv[2])
--- wxcloudrun/views/__init__.py
+++ wxcloudrun/views/__init__.py
@@ -1,1 +1,5 @@
 # 视图模块初始化文件
+# 导入所有视图模块，确保蓝图被注册
+from . import db_views
+from . import user_views  
+from . import chat_views
